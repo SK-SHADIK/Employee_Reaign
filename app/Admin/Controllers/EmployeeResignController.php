b56c4b48f2fa5e6cad39bd319093322892f63ec3
raihanislampolock <raihanpolock24@gmail.com>
2023-05-30 14:25:22 +0600

    Pull Request

--- app/Admin/Controllers/EmployeeResignController.php
+++ app/Admin/Controllers/EmployeeResignController.php
@@ -8,7 +8,6 @@ use Encore\Admin\Layout\Content;
 use Encore\Admin\Form;
 use Encore\Admin\Grid;
 use Encore\Admin\Show;
-
 use Illuminate\Http\Request;
 
 class EmployeeResignController extends AdminController
@@ -70,71 +69,39 @@ class EmployeeResignController extends AdminController
      * @return Form
      */
     protected function form()
-    {
-        $form = new Form(new EmployeeResign());
+{
+    $form = new Form(new EmployeeResign());
 
-        // $Employee = \App\Models\Employee::pluck('emp_id', 'id')->toArray();
-        // $form->select('employee_id', __('Employee_ID'))->options($Employee);
-        // $tools = \App\Models\EmployeeAccessTool::all();
+    $Employee = \App\Models\Employee::pluck('emp_id', 'id')->toArray();
+    $form->select('employee_id', __('Employee_ID'))->options($Employee);
+    $tools = \App\Models\EmployeeAccessTool::all();
 
-        // foreach ($tools as $tool) {
-        //     $form->text("Tool Name")->value($tool->tool)->readonly();
-        //     $form->switch('had_access', __('Had access'));
-        //     $form->switch('access_removed', __('Access removed'));
-        //     $form->text('remarks', __('Remarks'));
-        // }
-
-        // $form->saving(function (Form $form) {
-        //     $employeeId = $form->input('employee_id');
-        //     $toolName = $form->input('Tool Name');
-        //     $hadAccess = $form->input('had_access');
-        //     $accessRemoved = $form->input('access_removed');
-        //     $remarks = $form->input('remarks');
-            
-        //     $resignObj = new \App\Models\EmployeeReaignTool();
-        //     $employeeAccessTool = [
-        //         'employee_id' => $employeeId,
-        //         'employee_access_tool_id' => 1,
-        //         'had_access' => $hadAccess,
-        //         'access_removed' => $accessRemoved,
-        //         'remarks' => $remarks,
-        //     ];
-            
-        //     $resignObj->create($employeeAccessTool);
-        // });
-        
-        // $form->hidden('cb', __('Cb'))->value(auth()->user()->name);
-        // $form->hidden('ub', __('Ub'))->value(auth()->user()->name);
-        $employees = \App\Models\Employee::all();
-        $tools = \App\Models\EmployeeAccessTool::all(); 
-
-        return view('resign', ['employees' => $employees, 'tools' => $tools]);
+    foreach ($tools as $tool) {
+        $form->text("Tool Name")->value($tool->tool)->readonly();
+        $form->switch('had_access', __('Had access'));
+        $form->switch('access_removed', __('Access removed'));
+        $form->text('remarks', __('Remarks'));
     }
-    // public function create(Content $content)
-    // {
-    //     $employees = \App\Models\Employee::all();
-    //     $tools = \App\Models\EmployeeAccessTool::all();        
 
-    //     return $content
-    //         ->title('Create')
-    //         ->view('resign', ['employees' => $employees, 'tools' => $tools]);
-    // }
-    // public function store(Request $request)
-    // {
-    //     dd($request);
-
-    //     // Create a new AccessRecord instance and set the values
-    //     $accessRecord = new EmployeeResign();
-    //     $accessRecord->employee_id = $request->input('employee_id');
-    //     $accessRecord->employee_access_tool_id = $request->input('access_tool_id');
-    //     $accessRecord->had_access = $request->has('had_access') ? true : false;
-    //     $accessRecord->access_removed = $request->has('access_removed') ? true : false;
-    //     $accessRecord->remarks = $request->input('remarks');
-
-    //     // Save the access record
-    //     $accessRecord->save();
-
-    //     // Redirect back or to a success page
-    //     return redirect()->back()->with('success', 'Access record saved successfully.');
-    // }
+    $form->saving(function (Form $form) {
+        $employeeId = $form->input('employee_id');
+        $toolName = $form->input('Tool Name');
+        $hadAccess = $form->input('had_access');
+        $accessRemoved = $form->input('access_removed');
+        $remarks = $form->input('remarks');
+        
+        $employeeAccessTool = new EmployeeReaignTool(); // Create an instance of EmployeeReaignTool
+        $employeeAccessTool->employee_id = $employeeId;
+        $employeeAccessTool->employee_access_tool_id = 1;
+        $employeeAccessTool->had_access = $hadAccess;
+        $employeeAccessTool->access_removed = $accessRemoved;
+        $employeeAccessTool->remarks = $remarks;
+        
+        $employeeAccessTool->save(); // Save the employee access tool record
+    });
+    
+    $form->hidden('cb', __('Cb'))->value(auth()->user()->name);
+    $form->hidden('ub', __('Ub'))->value(auth()->user()->name);
+    return $form;
+}
 }
